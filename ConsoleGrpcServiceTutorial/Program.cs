50f0143c616a23ffa761c1fa1a9cae6915a972a6
Marouane <elmabroukmarouane@gmail.com>
2022-05-13 22:42:46 +0200

    Commit the final version of tutorial

---
+++ ConsoleGrpcServiceTutorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleGrpcServiceTutorial
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter the name request ...");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter your message ...");
+            var message = Console.ReadLine();
+            using (var channel = GrpcChannel.ForAddress("https://localhost:5001"))
+            {
+                var client = new Greeter.GreeterClient(channel);
+                var reply = await client.CallServiceGrpcAsync(new CallServiceGrpcoRequest
+                {
+                    Name = name,
+                    Message = message
+                });
+                Console.WriteLine("Message from GRPC Call Service : " + reply.Message);
+                Console.WriteLine("Press any key to exit ...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
